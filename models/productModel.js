058c33d05f383aeb8ddc2192e07268986451357b
JaneM23 <mendezjaneth1721@gmail.com>
2025-09-23 21:29:54 -0600

    feat: agregar juego F1 24 con imagen y precio

--- models/productModel.js
+++ models/productModel.js
@@ -3,4 +3,5 @@ module.exports = [
   { productId: '1', nombre: 'The Witcher 3', precio: 29.99, stock: 99, image: '/images/witcher.png' },
   { productId: '2', nombre: 'Cyberpunk 2077', precio: 59.99, stock: 99, image: '/images/cyberpunk.png' },
   { productId: '3', nombre: 'Minecraft',     precio: 19.99, stock: 99, image: '/images/minecraft.png' },
+  { productId: '4', nombre: 'F1 24', precio: 69.99, stock: 99, image: '/images/f1-24.png' },
 ];
